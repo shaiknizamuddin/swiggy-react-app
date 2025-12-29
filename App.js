4b0be72678eda494bc5949e41d97ee2e46efe6df
nizamuddin_shaik <nizamuddin_shaik@epam.com>
2025-12-29 22:14:39 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const reactElement = (
+  <div className="container">
+    <h1>Hello, World!</h1>
+    <p>Welcome to my React application.</p>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(reactElement);
