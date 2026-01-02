a328d63c5ea62288dfdbd3bd0ff7396dff5ca856
nizamuddin_shaik <nizamuddin_shaik@epam.com>
2026-01-03 01:15:04 +0530

    Added shimmerUI , copying data using useState variable, getting data from API and rendering it

--- components/Body.js
+++ components/Body.js
@@ -1,13 +1,37 @@
 import { restaurantsList } from "../shared/swiggyApi";
 import RestaurantCards from "./Restaurantcards";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [filteredRes, setFilteredRes] = useState(restaurantsList);
+  const [filteredRes, setFilteredRes] = useState([]);
+  const [apiDataCopy, setApiDataCopy] = useState([]);
+
+  useEffect(() => {
+    getResListFromApi();
+  }, []);
+  async function getResListFromApi() {
+    const apiUrl =
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.458489649946085&lng=78.39860314785044&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+    const response = await fetch(apiUrl);
+    const apiResponse = await response.json();
+
+    const finalData =
+      apiResponse?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+    console.log(finalData, "apiResponse++++");
+    setApiDataCopy(finalData);
+    setFilteredRes(finalData);
+  }
+
   let filteredList = [];
 
+  const clearFilters = () => {
+    setFilteredRes(apiDataCopy);
+  };
+
   const filterTopRatedRes = (value) => {
-    filteredList = restaurantsList.filter(
+    filteredList = filteredRes.filter(
       (restaurant) => restaurant?.info?.avgRating >= value
     );
     setFilteredRes(filteredList);
@@ -15,7 +39,11 @@ const Body = () => {
 
   const onSearchRes = (e) => {
     const searchText = e.target.value.toLowerCase();
-    filteredList = restaurantsList.filter(
+    if (searchText.length === 0) {
+      setFilteredRes(apiDataCopy);
+      return;
+    }
+    filteredList = filteredRes.filter(
       (restaurant) =>
         restaurant?.info?.name.toLowerCase().includes(searchText) ||
         restaurant?.info?.cuisines.join(" ").toLowerCase().includes(searchText)
@@ -23,17 +51,25 @@ const Body = () => {
     setFilteredRes(filteredList);
   };
 
+  if (filteredRes.length === 0) {
+    // return (
+    //   <div style={{ display: "flex", flexWrap: "wrap" }}>
+    //     {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((e, i) => {
+    //       return <Shimmer key={i} />;
+    //     })}
+    //   </div>
+    // );
+    return <Shimmer />;
+  }
+
   return (
     <div
       className="body"
       style={{ fontFamily: "Arial, Helvetica, sans-serif" }}
     >
       <div className="search-bar">
-        <button
-          style={{ padding: "20px" }}
-          onClick={() => filterTopRatedRes(0)}
-        >
-          Show All Restaurants
+        <button style={{ padding: "20px" }} onClick={() => clearFilters()}>
+          Clear Filters
         </button>
         <button
           style={{ padding: "20px" }}
